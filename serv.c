088e9f780c854aa610aabbe04b8e720261961926
rudreais <valentin.lattay@rudreais.com>
2018-10-01 14:07:33 +0200

    added bind

--- serv.c
+++ serv.c
@@ -1,3 +1,15 @@
+#include <string.h>
+
+/**
+* memset(void *s, int c, size_t n)
+**/
+
+#include <stdlib.h>
+
+/**
+* #define EXIT_FAILURE
+**/
+
 #include <sys/socket.h>
 
 /**
@@ -23,10 +35,10 @@
 ** <sys/socket.h>
 ** <netinet/in.h>
 * inet_aton(const char *cp, struct in_addr *inp)
+* htons(uint16_t hostshort)
 **/
 
 #include <netinet/in.h>
-
 /**
 * struct sockaddr_in {
 	sa_family_t sin_family; // famille d'adresse : AF_INET
@@ -48,7 +60,23 @@
 * close sockets
 **/
 
+#define LOCAL_PORT 10000
+#define LOCAL_HOST 127.0.0.1
+
 int main(void)
 {
+	int server_socket = socket(PF_INET, SOCK_STREAM, 0); // opening a socket
+	struct sockaddr_in server_socket_name; // struct containing information about the socket
+	int client_socket; // client's socket
+	int optval = 1;
+
+	if (server_socket < 0)
+		return EXIT_FAILURE;
+	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR,&optval, sizeof(int)); // make socket reuseable after closing server
+	memset((char *) &server_socket_name, 0, sizeof(struct sockaddr_in)); // giving enough memory to the struct sockaddr_in
+	server_socket_name.sin_family = AF_INET;
+	server_socket_name.sin_port = htons(LOCAL_PORT); // converting port
+	bind(server_socket, (struct sockaddr *) &server_socket_name, sizeof(struct sockaddr_in)); // binding socket
+	server_socket_name.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 	return 0;
 }
